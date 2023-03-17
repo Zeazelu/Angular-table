88ed0c5e783e1fb35e9a53013ff44135b959bc72
Zeazelu <50877478+Zeazelu@users.noreply.github.com>
2023-03-17 21:40:58 +0100

    polish font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import raports from './files/raport.json';
 import jsPDF from 'jspdf';
-import 'jspdf-autotable';
+import  'jspdf-autotable';
 import * as XLSX from 'xlsx';
+import { fontString } from './font/font';
+import html2canvas from 'html2canvas';
 
 
 
@@ -52,84 +54,46 @@ export class AppComponent {
   } [] = raports;
   selectedColumns: string[] = []; 
 
-  sum1 = 0.00;
-  sum2 = 0.00;
-  sum3 = 0.00;
-  sum4 = 0.00;
-  sum5 = 0.00;
-  sum6 = 0.00;
-  sum7 = 0.00;
-  sum8 = 0.00;
-  sum9 = 0.00;
-  sum10 = 0.00;
-  sum11 = 0.00;
-  sum12 = 0.00;
-  sum13 = 0.00;
+  sums: number[] = Array.from({length: 13}, () => 0);
 
   count() {
-    this.sum1 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.redemptionOfRepayments);
-    }, 0.00);
+    this.sums[0] = this.raportList.reduce((acc, val) => acc + parseFloat(val.redemptionOfRepayments), 0.00);
   }
   count2() {
-    this.sum2 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.sumOfRepayments);
-    }, 0.00);
+    this.sums[1] = this.raportList.reduce((acc, val) => acc + parseFloat(val.sumOfRepayments), 0.00);
   }
   count3() {
-    this.sum3 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.capital);
-    }, 0.00);
+    this.sums[2] = this.raportList.reduce((acc, val) => acc + parseFloat(val.capital), 0.00);
   }
   count4() {
-    this.sum4 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.contractualTermInterest);
-    }, 0.00);
+    this.sums[3] = this.raportList.reduce((acc, val) => acc + parseFloat(val.contractualTermInterest), 0.00);
   }
   count5() {
-    this.sum5 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.overdueContractualInterest);
-    }, 0.00);
+    this.sums[4] = this.raportList.reduce((acc, val) =>  acc + parseFloat(val.overdueContractualInterest), 0.00);
   }
   count6() {
-    this.sum6 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.sumOfContractualInterest);
-    }, 0.00);
+    this.sums[5] = this.raportList.reduce((acc, val) => acc + parseFloat(val.sumOfContractualInterest), 0.00);
   }
   count7() {
-    this.sum7 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.penaltyInterest);
-    }, 0.00);
+    this.sums[6] = this.raportList.reduce((acc, val) => acc + parseFloat(val.penaltyInterest), 0.00);
   }
   count8() {
-    this.sum8 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.interestInCourt);
-    }, 0.00);
+    this.sums[7] = this.raportList.reduce((acc, val) => acc + parseFloat(val.interestInCourt), 0.00);
   }
   count9() {
-    this.sum9 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.totalInterest);
-    }, 0.00);
+    this.sums[8] = this.raportList.reduce((acc, val) => acc + parseFloat(val.totalInterest), 0.00);
   }
   count10() {
-    this.sum10 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.commission);
-    }, 0.00);
+    this.sums[9] = this.raportList.reduce((acc, val) => acc + parseFloat(val.commission), 0.00);
   }
   count11() {
-    this.sum11 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.bailiffCommission);
-    }, 0.00);
+    this.sums[10] = this.raportList.reduce((acc, val) => acc + parseFloat(val.bailiffCommission), 0.00);
   }
   count12() {
-    this.sum12 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.overpaymentOfCapitalPeriod);
-    }, 0.00);
+    this.sums[11] = this.raportList.reduce((acc, val) => acc + parseFloat(val.overpaymentOfCapitalPeriod), 0.00);
   }
   count13() {
-    this.sum13 = this.raportList.reduce((acc, val) => {
-      return acc + parseFloat(val.othersInCourt);
-    }, 0.00);
+    this.sums[12] = this.raportList.reduce((acc, val) => acc + parseFloat(val.othersInCourt), 0.00);
   }
 
   @ViewChild('content', {static:false}) el!: ElementRef
@@ -141,38 +105,30 @@ export class AppComponent {
     a.download = 'raport.json';
     a.click();
   }
-  
-makePdf() {
-  const date = new Date().toLocaleString();
-  if(this.selectedColumns.length > 5) {
-    const pdf = new jsPDF("landscape", "mm", [350, 210]);
-    const header = function(data: any) {
-      pdf.setFontSize(12);
-      pdf.text('Raport splat klientow (PBaza) Prog. 20T_07 utworzony', 110, 15);
-      pdf.text(date, 215, 15);
-      pdf.text('Za okres: Od wlacznie 2022-12-02 Do 2022-12-21 wlacznie' , 125, 30);
-      pdf.text('Sprawdzil..................................', 15, 100);
-      pdf.text('Zatwierdzil..................................', 15, 115);
-    };
-    
-    (pdf as any).autoTable({ html: '#content', didDrawPage: header, startY: 40, });
-    pdf.save('table.pdf');
-    } else {
-    const pdf = new jsPDF();
-    const header = function(data: any) {
-      pdf.setFontSize(12);
-      pdf.text('Raport splat klientow (PBaza) Prog. 20T_07 utworzony' , 40, 15);
-      pdf.text(date, 145, 15);
-      pdf.text('Za okres: Od wlacznie 2022-12-02 Do 2022-12-21 wlacznie' , 50, 20);
-      pdf.text('Sprawdzil..................................', 15, 100);
-      pdf.text('Zatwierdzil..................................', 15, 115);
-    };
-    
-    (pdf as any).autoTable({ html: '#content', didDrawPage: header, startY: 40, });
-    pdf.save('table.pdf');
+
+    makePdf2() {
+      const date = new Date().toLocaleString();
+      const element = document.getElementById('pdf')!;
+      html2canvas(element, { scale: 3 }).then(canvas => {
+        const contentDataURL = canvas.toDataURL('image/png')
+        let pdf = new jsPDF("landscape", "mm", [350, 210]);
+        var width = pdf.internal.pageSize.getWidth();
+        var height = canvas.height * width / canvas.width;
+        pdf.addFileToVFS("Abha.ttf", fontString);
+        pdf.addFont('Abha.ttf', 'Abha', 'normal', 'Identity-H');
+        pdf.setFont('Abha');
+        pdf.setFontSize(12);
+        pdf.text('Raport spłat klientów (PBaza) Prog. 20T_07 utworzony', 110, 15);
+        pdf.text(date, 210, 15);
+        pdf.text('Za okres: Od włącznie 2022-12-02 Do 2022-12-21 włącznie' , 125, 30);
+        pdf.addImage(contentDataURL, 'PNG', 0, 50, width, height);
+        pdf.text('Sprawdził..................................', 15, 100);
+        pdf.text('Zatwierdził..................................', 15, 115);
+        pdf.save('table.pdf'); 
+       
+      });
     }
-  }
-  excelHeaders:string[] = ["Name","Age","Email","Contact Number","Location"];
+  
   makeExcel() : void 
   {
     let element = document.getElementById('content');
